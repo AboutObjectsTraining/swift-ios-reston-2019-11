dcc38d25babfd0a2f8e778c9434ee4944ae2b760
Jonathan Lehr <jonathan@aboutobjects.com>
2019-11-04 16:29:32 -0500

    Added swift-programming solution code

---
+++ Labs/Labs/main.swift
@@ -0,0 +1,12 @@
+// Copyright (C) 2019 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+import Foundation
+
+print("Hello, World!")
+
+func foo() {
+    print("Whee!")
+}
+
+foo()
---
+++ Labs2/Labs2/Labs2.h
@@ -0,0 +1,14 @@
+// Copyright (C) 2019 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Labs2.
+FOUNDATION_EXPORT double Labs2VersionNumber;
+
+//! Project version string for Labs2.
+FOUNDATION_EXPORT const unsigned char Labs2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Labs2/PublicHeader.h>
+
+
